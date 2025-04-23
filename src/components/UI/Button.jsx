814a285f4ec9ce277917e433d1b237ebd697740d
Aiganysh <dzhumagulova8@gmail.com>
2025-04-24 00:54:13 +0300

    create auth content & saving

---
+++ src/components/UI/Button.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+const StylesButton = (variant) => {
+  switch (variant) {
+    case "change": {
+      return {
+        width: "98px",
+        height: "40px",
+        fontSize: "14px",
+        fontWeight: "700",
+        color: "#ffffff",
+        borderRadius: "3px",
+        background: "rgb(70, 163, 88)",
+      };
+    }
+    case "remove": {
+      return {
+        width: "74px",
+        height: "40px",
+        fontSize: "14px",
+        fontWeight: "400",
+        borderRadius: "3px",
+        background: "#ffffff",
+      };
+    }
+    default:
+      return null;
+  }
+};
+export const Button = ({ children, type, onClick, variant, ...rest }) => {
+  return (
+    <StyledButton type={type} onClick={onClick} variant={variant} {...rest}>
+      {children}
+    </StyledButton>
+  );
+};
+const StyledButton = styled.button`
+  border: none;
+  cursor: pointer;
+  ${(props) => StylesButton(props.variant)}
+`;
